3aa7c5335a14ddca06ca447a57cd53cd665d3a86
David Felix <david.felix02@hotmail.com>
2020-09-18 02:56:12 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,23 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
+//Mines works but it is not passing
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = tutorials.map((tutorial) => {
+//   words = tutorial.split(" ")
+//   let caps = words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+//   let got = caps.join(" ");
+//   return got;
+//   });
+// console.log(titleCased)
+
+const titleCased = (input) => {
+  return tutorials.map( line => {
+    let tokens = line.split(' ')
+    let capitalizedTokens =
+      tokens.map( token => token.charAt(0).toUpperCase() + token.slice(1) )
+    let response = capitalizedTokens.join(' ')
+    return response
+  })
 }
+console.log(titleCased)
